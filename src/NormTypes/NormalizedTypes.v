bfa93499327c0cc377f986a5003495bd2671d8a1
Pierre Courtieu <Pierre.Courtieu@cnam.fr>
2019-09-19 17:53:25 +0200

    Lemmas about lift.

--- src/NormTypes/NormalizedTypes.v
+++ src/NormTypes/NormalizedTypes.v
@@ -5,7 +5,7 @@ Require Import NormTypes.irreleventQuantifiers.
 Section Norm.
   Context {A B:Type}.
   Variable nf: A -> B.
-  Definition Norm := { b | ! (exists a, ! b = nf a) }. (* irrelevant existential proof. *)
+  Definition Norm := { b | irr (iexists a, b = nf a) }. (* irrelevant existential proof. *)
   (* Given a:A, its equivalence class is given by (nf a). *)
   Definition class (a:A):Norm := isig_intro _ (nf a) (iex_intro _ a eq_refl).
 
@@ -17,6 +17,40 @@ Section Norm.
   Definition lift (X:Type) (f:B -> X): Norm -> X := fun a:Norm => f (extract a).
 
   Definition liftR:= fun (R:B -> B -> Prop) => fun a b:Norm => R (extract a) (extract b).
+
+  (* Lemma facile (P:B -> Prop):     forall b: B, P b -> forall a:A, nf a = b -> lift _ P (class a). *)
+  Lemma facile: forall (C:Type) (f:B -> C),
+    forall b: B, forall a:Norm, extract a = b -> lift _ f a = f b.
+  Proof.
+    intros C f b a H. 
+    unfold lift.
+    rewrite H.
+    reflexivity.
+  Qed.
+
+  Lemma facile2C (C:Type) (f:B -> B -> C):
+    forall b b': B, forall a a':Norm, extract a = b ->
+                                      extract a' = b'
+                                      -> lift _ ((lift _ f a)) a' = f b b'.
+  Proof.
+    intros b b' a a' H H0. 
+    erewrite facile.
+    2:eassumption.
+    erewrite facile.
+    2:eassumption.
+    reflexivity.
+  Qed.
+
+
+  Lemma facile2 (P:B -> B -> Prop):
+    forall b b': B, P b b' -> forall a a':Norm, extract a = b ->
+                                                extract a' = b'
+                                                -> lift _ ((lift _ P a)) a'.
+  Proof.
+    intros b b' H a a' H0 H1. 
+    erewrite facile2C;eauto.
+  Qed.
+
 
   Definition eqS := (liftR eq).
 
--- src/NormTypes/tests.v
+++ src/NormTypes/tests.v
@@ -1,32 +1,68 @@
 Require Import NormTypes.NormalizedTypes.
+Require Import NormTypes.irreleventQuantifiers.
 Require Import NPeano Omega.
 
 Module Mod3_byHand.
-  Fixpoint mod3 (n:nat) :=
-    match n with
-    | S (S (S n')) => mod3 n'
-    | _ => n
-    end.
-  Definition NMod3 := Norm mod3.
+  Definition mod3 (n:nat) := (Nat.modulo n 3).
 
+  (* Notation "'class' nf a" := (isig_intro _ (nf a) (iex_intro _ a eq_refl)) (at level 100). *)
   Check (class mod3 3).
   Check (class mod3 0).
 
-  Notation "'[[' x ']Z/3Z]'" := (class mod3 x) (at level 200).
-  Check [[3]Z/3Z].
-  Check [[0]Z/3Z].
+  Notation "'[[' x ']N/3N]'" := (class mod3 x) (at level 200).
+  Notation "'N/3N'" := (Norm mod3) (at level 200).
+  Check [[3]N/3N].
+  Check [[0]N/3N].
 
 
-  Lemma eq_3_0: [[3]Z/3Z] = [[0]Z/3Z].
+  Lemma eq_3_0: [[3]N/3N] = [[0]N/3N].
   Proof.
     reflexivity.
   Qed.
 
-  Lemma eq_3_x: forall x , [[x]Z/3Z] = [[3+x]Z/3Z].
+  Lemma eq_3_x: forall x , [[x]N/3N] = [[3+x]N/3N].
   Proof.
+    intros x. 
+    apply eqS_eq.
     simpl.
-    reflexivity.
+    unfold class.
+    unfold eqS, liftR.
+    simpl.
+    replace (S (S (S x))) with (x+1*3);try omega.
+    unfold mod3.
+    rewrite (Nat.mod_add x 1 3);auto.
   Qed.
+
+  Lemma eq_3_mult_x: forall x , [[0]N/3N] = [[3*x]N/3N].
+  Proof.
+    intros x.
+    apply eqS_eq.
+    simpl.
+    unfold class.
+    unfold eqS, liftR.
+    simpl.
+    replace (x + (x + (x + 0))) with (0+x*3);try omega.
+    unfold mod3.
+
+    rewrite (Nat.mod_add 0 x 3);auto.
+  Qed.
+
+  Definition add1:= @lift _ _ mod3 _ (fun x:nat => [[ x +1 ]N/3N]).
+  Eval compute in (add1 ([[3]N/3N])).
+
+  Notation "[[ x ]]" := (isig_intro _ x _) (only printing, at level 200).
+  Eval compute in (add1 ([[3]N/3N])).
+  Eval compute in (add1 ([[4]N/3N])).
+
+  Definition add:=
+    @lift _ _ mod3 _ (fun n:nat =>
+                            @lift _ _ mod3 _ (fun x:nat => [[ x + n ]N/3N])).
+
+  Eval compute in (add ([[2]N/3N]) ([[2]N/3N])).
+
+  (* Definition toint (x:N/3N):nat := extract _ x. *)
+  (* Definition fromint (x:nat):N/3N := class _ x. *)
+
 End Mod3_byHand.
 
 
@@ -65,10 +101,11 @@ Module Mod3_usingLibMod.
 
 End Mod3_usingLibMod.
 
+(* Example: Z/3Z. *)
+Require Import ZArith.
+
 Module Mod3Z.
 
-  (* Example: Z/3Z. *)
-  Require Import ZArith.
   Open Scope Z_scope.
 
   Definition mod3Z (z:Z) := (Z.modulo z 3%Z).
